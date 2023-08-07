af1ce5f58ee8ad1e11ce8a09bd31ef26ffc6b5b6
Remy102 <remys0450@gmail.com>
2023-08-07 18:51:52 +0300

    feat: setup init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NestedTableComponent } from './components/nested-table/nested-table.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'nested-table', component: NestedTableComponent },
+  { path: '', redirectTo: '/nested-table', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NestedTableComponent } from './components/nested-table/nested-table.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NestedTableComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
