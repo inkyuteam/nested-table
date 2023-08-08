4bfd41edf6b731ae2d0b7a9a9bcbaa7eb8d6075b
Remy102 <remys0450@gmail.com>
2023-08-08 23:01:28 +0300

    feat: integrate bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NestedTableComponent } from './components/nested-table/nested-table.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/nested-table/nested-table.component.ts
+++ src/app/components/nested-table/nested-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { data } from 'src/app/config';
 import { TableData } from 'src/app/interfaces';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nested-table',
@@ -10,10 +11,15 @@ import { TableData } from 'src/app/interfaces';
 export class NestedTableComponent {
   @Input() tableData?: TableData[];
 
+  constructor(private modalService: NgbModal) {}
   ngOnInit() {
     if (!this.tableData) {
       this.tableData = data;
     }
     this.tableData.forEach(row => row.isExpanded = false);
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
   }
 }
